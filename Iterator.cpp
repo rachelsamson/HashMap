92c8f793b3dcd92a4f424051927a5b9aa7724cb3
rachelsamson <36596746+rachelsamson@users.noreply.github.com>
2018-07-19 11:54:33 +0530

    Add files via upload

--- Iterator.cpp
+++ Iterator.cpp
@@ -4,68 +4,71 @@
  *  Created on: Jul 18, 2018
  *      Author: samsor1
  */
+
+//checking for errors
 #include<iostream>
 #include "Iterator.h"
 using namespace std;
 template<class T1,class T2>
-Iterator<T1,T2>::Iterator(const T1& a,const T2& b)
-{
-	//this->Node.key=a;
-	//this->Node.value=b;
+Iterator<T1,T2>::Iterator(){}
 
-}
 template<class T1,class T2>
-bool Iterator<T1,T2>::operator ++(const Iterator<T1, T2>& x, const Iterator<T1, T2>& y)
+Iterator<T1,T2>::Iterator(const Iterator<T1,T2>& x)
 {
-	//Linked list tarversal
-	//Iterator::Node current;
-	//Iterator Obj_It=*this;
-	//current=current.linked;
-	//current.link++;
-	return 0;
-
+	this->Node.key=x.Key;
+	this->Node.value=x.Value;
+	this->Node.next=NULL;
 }
 
 template<class T1,class T2>
-void Iterator<T1,T2>::m_begin()
+Node Iterator<T1,T2>::m_begin()
 {
-	//node traversal from beginning (array list traversal)
-	Iterator::Node current;
-	int *p =current[0];
-	for(int i =0;i<sizeof(current)/sizeof(current[0]);i++)
+	 Node *data=lnode;
+	return data;
+
+
+}
+
+template<class T1,class T2>
+Node Iterator<T1,T2>::m_end()
+{
+	Node *p=this;
+	 while(p->next!=NULL)
 	 {
-		cout<<current[i];
+		 p=p->next;
 	 }
 
-
-
+	 return p;
 
 }
+template<class T1,class T2>
+T1 Iterator<T1,T2>::operator ++(const Iterator<T1,T2> x)
+{
+	T1 i;
+	 for ( i = m_begin().Key; i != m_end().Key; ++i)
+	        {
+		   return i;
+	        }
+
+	return i;
+
+}
+
 
 template<class T1,class T2>
-bool Iterator<T1,T2>::operator!=(const Iterator<T1, T2>& x,const Iterator<T1, T2>& y)
+bool Iterator<T1,T2>::operator!=()
 {
-//	index=y.Node.hashValue%Max;
-	//for(int i =index;i<sizeof(lnode)/sizeof(lnode[0]);i++)
+	 T1 i;
+	 for (i = m_begin().Key; i != m_end().Key; ++i)
+	 {
+		            if (i == this)
+		            {
+		                return true;
+		            }
+	 }
+ return false;
 
-	if( x.lnode[index].key==y.Node.key && x.lnode[index].value==y.Node.value)
-	{
-		for(int j=0;j<sizeof(anode)/sizeof(anode[0]);j++)
-		{
-		  if( x.key==y.Node.key && x.anode[j].value==y.Node.value)
-		  {
-			  break;
-		 }
-		  else
-		  {
-			  j++;
-		  }
-
-
-		}
-	}
 }
-
 
 
 
--- Iterator.h
+++ Iterator.h
@@ -13,25 +13,27 @@ class Iterator
  private:
 	typedef struct Node
 	{
-		T1 key;
-		T2 value;
+		typedef T1 Key;
+		typedef T2 Value;
 		int hashValue;
-		Node *next;
-		Node *linked;
+		int *next;
+		//Node *end;
 
 	};
 	Node anode[50],lnode[50];
 	struct Node *ptr = anode;
 	struct Node *prtl=lnode;
+	//typedef std::pair<const T1, T2> value_type;
+
  public:
-	Iterator(){}
-	Iterator(const T1&,const T2&);
+	Iterator();
+	Iterator(const Iterator<T1,T2>&);
 	bool operator++(const Iterator<T1,T2>&);
-	bool operator!=(const Iterator<T1, T2>&);
+	bool operator!=(const Iterator<T1,T2>&);
 	// operator * ();
 		//int m_access(string,node);
-	void m_begin();
-	void m_end();
+	Node m_begin();
+	Node m_end();
 	virtual ~Iterator(){}
 };
 
