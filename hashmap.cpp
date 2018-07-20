0e11636998a765f7d440dd632e9f744eb3eaffbf
vivekraviiyengar <pakshiraja.brilliant@gmail.com>
2018-07-20 17:10:37 +0530

    Add files via upload

--- hashmap.cpp
+++ hashmap.cpp
@@ -4,50 +4,55 @@
  *  Created on: Jul 18, 2018
  *      Author: raviiv
  */
-//Checking for errors In Progress
+//stringstream concept
+
 #include "hashmap.h"
 using namespace std;
 //#include<vector>
 template<typename K,typename V>
-hashnode<K,V>::hashnode(K Key,V Value)
+hashnode<K,V>::hashnode(K key,V value)
 {
+	head=NULL;
+	tail=NULL;
+	int size=0;
+	hashnode<K,V> obj_arr=new hashnode<K,V>*[CAPACITY];//initializing
+	for(int i;i < CAPACITY; i++)
+		{
+			obj_arr[i]=NULL;
+		}
 	//this->Value=value;
 	//this->Key=Key;
+}
+/*template<typename K,typename V>
+//hashmap<K,V>::hashmap()
+{
+	//CAPACITY=26;
 
-}
+}*/
 template<typename K,typename V>
-hashmap<K,V>::hashmap()
+void hashnode<K,V>::insertNode(K Key ,V Value)
+
 {
-	capacity=26;
-	size=0;
-	obj_arr=new hashnode<K,V>*[capacity];
-	for(int i;i < capacity; i++)
-	{
-		obj_arr[i]=NULL;
-	}
-}
-template<typename K,typename V>
-void hashmap<K,V>::insertNode(K Key ,V Value)
-{
+	this->node.Key=Key;
+	this->node.Value=Value;
 	hashnode<K,V> *tmp=new hashnode<K,V>(Key,Value);
 	int hashIndex=hashCode(Key);
-	while(obj_arr[hashIndex]!=NULL || obj_arr[hashIndex].Key!=Key || obj_arr[hashIndex].Value!=Value)
+	while(tmp->lnode[hashIndex]!=NULL || tmp->lnode[hashIndex].Key!=Key || tmp->lnode[hashIndex].Value!=Value)
 	{
-		if(obj_arr[hashIndex].HEAD == NULL)
+		if(tmp->lnode[hashIndex].HEAD == NULL && hashIndex == tmp->lnode[hashIndex])//i==a[i]
 		{
 			//creating new node
-			obj_arr[hashIndex]->lnode *next= new obj_arr[hashnode].lnode;
-			obj_arr[hashIndex].lnode.Key =tmp->hashnode.Key;
-
-			obj_arr[hashIndex].lnode.Value =tmp->hashnode.Value;
+			tmp->lnode[hashIndex].next= tmp->hashnode.lnode[hashIndex];
+			tmp[hashIndex].lnode.Key =tmp->hashnode.Key;
+			tmp[hashIndex].lnode.Value =tmp->hashnode.Value;
 		}
 	}
 			hashIndex++;
-			hashIndex%=capacity;
-	for(int i=0;i<capacity;i++)
+			hashIndex%=CAPACITY;
+	for(int i=0;i<CAPACITY;i++)
 	{
 		//obj_arr[hashIndex]==tmp->hashnode(Key)
-		if(obj_arr[i]!=NULL);
+		if(tmp[i]!=NULL);
 		{
 
 			//tmp->lnode[];
