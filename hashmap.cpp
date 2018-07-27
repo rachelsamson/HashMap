72edaeae5c638f212659094e2a8f400c765667fa
vivekraviiyengar <pakshiraja.brilliant@gmail.com>
2018-07-27 13:54:29 +0530

    Add files via upload

--- hashmap.cpp
+++ hashmap.cpp
@@ -8,7 +8,7 @@
 #include "hashheader.h"
 #include<stdlib.h>
 using namespace std;
-int count;
+//int count;
 template<typename T1,typename T2>
 hashmap<T1,T2>::hashmap()
 {
@@ -21,7 +21,7 @@ hashmap<T1,T2>::hashmap()
 {
 
 }*/
-template<typename T1>
+/*template<typename T1>
 int m_calculatehash(T1 key)
 {
 
@@ -33,41 +33,59 @@ int m_calculatehash(T1 key)
 template<typename T1,typename T2>
 bool hashmap<T1,T2>::m_Insert(T1 key,T2 value)
 {
-	int index=m_calculatehash(key);
-	hashnode<int,int> *newNode=new hashnode<int,int>;
-	newNode->key=key;
-	newNode->value=value;
-	struct hashnode<T1,T2> nodeptr[index]=(struct node*)malloc(sizeof(struct node));
-	if(nodeptr[index]==NULL)
+	cout<<"this is done";
+	int index=1;//m_calculatehash(key);
+	//struct hashnode<T1,T2> node=(struct node*)malloc(sizeof(struct node));
+	//struct hashnode<T1,T2> newNode=new hashnode<T1,T2>;
+	//node->key=key;
+	//node->value=value;
+	//struct hashnode<T1,T2> node=(struct node*)malloc(sizeof(struct node));
+	struct hashnode<T1,T2> newNode;//=new hashnode<T1,T2>;
+	newNode.key=key;
+	newNode.value=value;
+	hashnode<T1,T2> *to_newNode;
+	to_newNode=&newNode;
+	//newNode=node;
+
+	if(nodeptr[index].key==NULL)
 	{
-		nodeptr[index]->key=key;
-		nodeptr[index]->value=value;
-		nodeptr[index]->left = NULL;
-		nodeptr[index]->right = NULL;
-		count++;
+
+		nodeptr[index]=newNode;
+		//nodeptr[index]->key=key;
+		//nodeptr[index]->value=value;
+		nodeptr[index].left = NULL;
+		nodeptr[index].right = NULL;
+		//count++;
+		cout<<nodeptr[index].key;
 		return true ;
 	}
+
 	else
 	{
-		struct hashnode<T1,T2> node=(struct node*)malloc(sizeof(struct node));
-		node.key=key;
-		node.value=value;
+		//struct hashnode<T1,T2> node=(struct node*)malloc(sizeof(struct node));
+		//node=newNode;
 		hashnode<T1,T2> *temp;
 		hashnode<T1,T2> *back;
-		temp=nodeptr[index];
-		back=NULL;
+		temp=&nodeptr[index];
+		//back=NULL;
+		cout<<temp<<endl;
+		cout<<&nodeptr[index];
+		cout<<"this is while";
 		while(temp!=NULL)
 		{
+
 			back=temp;
-			if(temp->key>node.key)
+			if(temp->key>newNode.key)
 			{
-				temp->left=node;
-				count++;
+				nodeptr[index].left=to_newNode;
+				//count++;
+				cout<<nodeptr[index].key;
 			}
 			else
 			{
-				temp->right=node;
-				count++;
+				nodeptr[index].right=to_newNode;
+				cout<<nodeptr[index].key;
+				//count++;
 			}
 
 		}
